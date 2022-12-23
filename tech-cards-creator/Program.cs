dfd3813facb20ac21012d476453469ccc1f24762
Pavel Egorov <xoposhiy@mail.ru>
2022-12-23 23:22:54 +0400

    first commit

---
+++ tech-cards-creator/Program.cs
@@ -0,0 +1,163 @@
+﻿using System.Globalization;
+using Microsoft.Playwright;
+
+namespace tech_cards_creator
+{
+    internal class Program
+    {
+        public const int Year  = 2022;
+        public const int YearPart = 1;
+        public const int CourseYear = 4;
+        
+        // Нужно залогиниться вручную в браузере и скопировать из DevTools - Application - Cookie JSESSIONID:
+        public const string JsessionidCookie = "6DCC48E36DD3CB0E93164C4E39797BBA";
+
+        static async Task Main()
+        {
+            using var pw = await Playwright.CreateAsync();
+            await using var browser = await pw.Chromium.LaunchAsync(new BrowserTypeLaunchOptions()
+            {
+                Headless = false,
+                Timeout = 3000
+            });
+
+            var context = await browser.NewContextAsync();
+            var cookie = new Cookie
+            {
+                Name = "JSESSIONID",
+                Value = JsessionidCookie,
+                Domain = "brs.urfu.ru",
+                Path = "/mrd"
+            };
+            var page = await context.NewPageAsync();
+            await page.GotoAsync("https://brs.urfu.ru/mrd/mvc/mobile#/");
+            await context.AddCookiesAsync(new[] { cookie });
+
+            while (true)
+            {
+                await page.GotoAsync(
+                    $"https://brs.urfu.ru/mrd/mvc/mobile/view/technologyCard/{Year}/{YearPart}/{CourseYear}/its");
+                var candidate = page.GetByText("Нет");
+                await candidate.First.ClickAsync();
+                Console.WriteLine(await page.TitleAsync());
+                await NextButtonClick(page);
+                var buttons = page.Locator("#buttonpractice,#buttonlecture,#buttonlaboratory");
+                var (loadTypeCount, lectureIndex) = await GetLectureLoadIndex(buttons);
+                await buttons.Nth(0).ClickAsync();
+                await page.WaitForNavigationAsync();
+                for (var i = 0; i < loadTypeCount; i++)
+                {
+                    await FillLoadType(page);
+                    if (i < loadTypeCount - 1) 
+                        await OpenLoadTypeSettings(page, i+1);
+                }
+
+                await NextButtonClick(page);
+                var inputFactors = page.Locator(".input-factor");
+                var inputFactorsCount = await inputFactors.CountAsync();
+                var iFactor = 0;
+                // Заполняем таблицу по три числа в строке, по строке на каждую loadType
+                while (iFactor < inputFactorsCount)
+                {
+                    if (iFactor / 3 == lectureIndex)
+                        await SetFactors(inputFactors, iFactor, 0.5, 0.5, 1.0 / loadTypeCount);
+                    else
+                        await SetFactors(inputFactors, iFactor, 1, 0, 1.0 / loadTypeCount);
+                    iFactor += 3;
+                }
+                
+                Thread.Sleep(1000);  // Появляются всякие крутилки, которые если быстро кликать зависают.
+                buttons = page.GetByText("Далее");
+                await buttons.ClickAsync(); // этот клик не срабатывает как клик, а только как потеря фокуса у редактирования.
+                await buttons.ClickAsync(); // поэтому есть второй Click
+                
+                // Тут должна быть плашка "Ошибок не обнаружено"
+
+                Thread.Sleep(1000);  // Появляются всякие крутилки, которые если быстро кликать зависают.
+                buttons = page.GetByText("Согласовать тех. карту");
+                await buttons.ClickAsync();
+                
+                // Это нужно, чтобы упасть, если не согласовалась ↓
+                await page.GetByText("Тех. карта успешно согласована.").FocusAsync(); 
+                Console.WriteLine("СОГЛАСОВАЛИ!");
+            }
+        }
+
+        private static async Task OpenLoadTypeSettings(IPage page, int loadTypeIndex)
+        {
+            await page.Locator("#technologyCardTypeId").ClickAsync();
+            var buttons = page.Locator("#buttonpractice,#buttonlecture,#buttonlaboratory");
+            await buttons.Nth(loadTypeIndex).ClickAsync();
+            await page.WaitForNavigationAsync();
+        }
+
+        private static async Task SelectLoadTypeSettings(IPage page, int loadTypeIndex)
+        {
+            var buttons = page.Locator("#buttonpractice,#buttonlecture,#buttonlaboratory");
+            await buttons.Nth(loadTypeIndex + 1).ClickAsync();
+            await page.WaitForNavigationAsync();
+        }
+
+        private static async Task<(int loadTypeCount, int lectureIndex)> GetLectureLoadIndex(ILocator buttons)
+        {
+            var loadTypeCount = await buttons.CountAsync();
+            var lectureIndex = 0;
+            for (var i = 0; i < loadTypeCount; i++)
+            {
+                var name = await buttons.Nth(i).InnerTextAsync();
+                if (name == "лекции")
+                    lectureIndex = i;
+            }
+
+            return (loadTypeCount, lectureIndex);
+        }
+
+        private static async Task SetFactors(ILocator inputFactors, int iFactor, double factorCurrent, double factorIntermediate, double loadTypeFactor)
+        {
+            await SetText(inputFactors.Nth(iFactor), factorCurrent.ToString(CultureInfo.CurrentCulture));
+            Thread.Sleep(100); // Появляются всякие крутилки, которые если быстро кликать зависают.
+            await SetText(inputFactors.Nth(iFactor + 1), factorIntermediate.ToString(CultureInfo.CurrentCulture));
+            Thread.Sleep(100);
+            await SetText(inputFactors.Nth(iFactor + 2), loadTypeFactor.ToString(CultureInfo.CurrentCulture));
+            Thread.Sleep(100);
+        }
+
+        private static async Task NextButtonClick(IPage page)
+        {
+            var button = page.GetByText("Далее");
+            await button.FocusAsync();
+            await button.ClickAsync();
+            await page.WaitForNavigationAsync();
+        }
+
+        private static async Task FillLoadType(IPage page)
+        {
+            var dz = page.Locator("#current-items li");
+            var kmCount = await dz.CountAsync() - 1;
+            if (kmCount == 0)
+            {
+                await page.GetByText("Добавить").ClickAsync();
+                await SetText(page.Locator("#name"), "Домашняя работа");
+                await SetText(page.Locator("#maxValue"), "100");
+                await page.GetByText("Сохранить").ClickAsync();
+            }
+            else
+            {
+                var value = 100 / kmCount;
+                for (int i = 0; i < kmCount; i++)
+                {
+                    await dz.Nth(i).ClickAsync();
+                    await SetText(page.Locator("#maxValue"), value.ToString());
+                    await page.GetByText("Сохранить").ClickAsync();
+                }
+            }
+        }
+
+        private static async Task SetText(ILocator editBox, string value)
+        {
+            // Вместо FillTextAsync, потому что он падает на попытке заполнить числовое поле.
+            await editBox.SelectTextAsync();
+            await editBox.TypeAsync(value);
+        }
+    }
+}
