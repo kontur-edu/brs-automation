d38943fbc6c4fce3356baa89eaa8dc312460e9f8
Pavel Egorov <xoposhiy@mail.ru>
2023-01-19 18:20:27 +0400

    Техкарты для Специальных курсов.

--- tech-cards-creator/Program.cs
+++ tech-cards-creator/Program.cs
@@ -1,5 +1,10 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
+using System.Net;
 using Microsoft.Playwright;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Cookie = Microsoft.Playwright.Cookie;
 
 namespace tech_cards_creator
 {
@@ -7,10 +12,14 @@ namespace tech_cards_creator
     {
         public const int Year  = 2022;
         public const int YearPart = 1;
-        public const int CourseYear = 4;
+        public const int CourseYear = 3;
         
         // Нужно залогиниться вручную в браузере и скопировать из DevTools - Application - Cookie JSESSIONID:
-        public const string JsessionidCookie = "6DCC48E36DD3CB0E93164C4E39797BBA";
+        public const string JsessionidCookie = "A3D0E4A5EEF73CA4A7A0DB2910FFA49A";
+
+        // На будущее: вероятно лучше полностью перейти на использование API.
+        // Потому что фронт нечеловечески неудобен для его обхода через любые средства автоматизации браузеров :(
+        // См, как реализовано получение всех дисциплин GetDisciplineTechCardsViaApi
 
         static async Task Main()
         {
@@ -18,28 +27,21 @@ namespace tech_cards_creator
             await using var browser = await pw.Chromium.LaunchAsync(new BrowserTypeLaunchOptions()
             {
                 Headless = false,
-                Timeout = 3000
+                Timeout = 60000
             });
 
             var context = await browser.NewContextAsync();
-            var cookie = new Cookie
-            {
-                Name = "JSESSIONID",
-                Value = JsessionidCookie,
-                Domain = "brs.urfu.ru",
-                Path = "/mrd"
-            };
             var page = await context.NewPageAsync();
             await page.GotoAsync("https://brs.urfu.ru/mrd/mvc/mobile#/");
-            await context.AddCookiesAsync(new[] { cookie });
-
-            while (true)
+            await AddAuthCookie(context);
+            var techCards = await GetDisciplineTechCardsViaApi(2022, 1, 3, false);
+            foreach (var techCard in techCards)
             {
-                await page.GotoAsync(
-                    $"https://brs.urfu.ru/mrd/mvc/mobile/view/technologyCard/{Year}/{YearPart}/{CourseYear}/its");
-                var candidate = page.GetByText("Нет");
-                await candidate.First.ClickAsync();
-                Console.WriteLine(await page.TitleAsync());
+                if (!techCard.CourseName.StartsWith("Специальный курс") || techCard.Agreed)
+                    continue;
+                Console.WriteLine("Create tech card for course: " + techCard);
+                await page.GotoAsync($"https://brs.urfu.ru/mrd/mvc/mobile/view/technologyCard/{techCard.Id}/intermediate#/");
+                await SelectExamLoadType(page);
                 await NextButtonClick(page);
                 var buttons = page.Locator("#buttonpractice,#buttonlecture,#buttonlaboratory");
                 var (loadTypeCount, lectureIndex) = await GetLectureLoadIndex(buttons);
@@ -72,16 +74,104 @@ namespace tech_cards_creator
                 await buttons.ClickAsync(); // поэтому есть второй Click
                 
                 // Тут должна быть плашка "Ошибок не обнаружено"
-
+                
                 Thread.Sleep(1000);  // Появляются всякие крутилки, которые если быстро кликать зависают.
                 buttons = page.GetByText("Согласовать тех. карту");
-                await buttons.ClickAsync();
-                
-                // Это нужно, чтобы упасть, если не согласовалась ↓
-                await page.GetByText("Тех. карта успешно согласована.").FocusAsync(); 
-                Console.WriteLine("СОГЛАСОВАЛИ!");
+                if (await buttons.CountAsync() > 0)
+                {
+                    await buttons.First.ClickAsync();
+
+                    // Это нужно, чтобы упасть, если не согласовалась ↓
+                    await page.GetByText("Тех. карта успешно согласована.").FocusAsync();
+                    Console.WriteLine("СОГЛАСОВАЛИ!");
+                }
+                else
+                {
+                    Console.WriteLine("Готова к согласованию");
+                }
             }
         }
+
+        private static async Task AddAuthCookie(IBrowserContext context)
+        {
+            var cookie = new Cookie
+            {
+                Name = "JSESSIONID",
+                Value = JsessionidCookie,
+                Domain = "brs.urfu.ru",
+                Path = "/mrd"
+            };
+            await context.AddCookiesAsync(new[] { cookie });
+        }
+
+        /// <summary>
+        /// Реализовано без браузера через API, потому что так оказалось проще вытащить идентификаторы.
+        /// Этот запрос самый тяжелый в БРС, поэтому хочется сделать его один раз, и запомнить все идентификаторы курсов,
+        /// а не повторять этот запрос после обработки каждого курса.
+        /// </summary>
+        /// <param name="year">2022 - астрономический год, в котором начался учебный год</param>
+        /// <param name="yearPart">1 для осеннего или 2 для весеннего</param>
+        /// <param name="courseYear">1-4 - год обучения студентов</param>
+        /// <returns></returns>
+        private static async Task<List<TechCardItem>> GetDisciplineTechCardsViaApi(int year, int yearPart, int courseYear, bool its)
+        {
+            var path = its ? "module/fetch" : "discipline/page";
+            var url = $"https://brs.urfu.ru/mrd/mvc/mobile/technologyCard/{path}?year={year}&termType={yearPart}&course={courseYear}&page=1&pageSize=100&search=";
+
+            var cookieContainer = new CookieContainer();
+            cookieContainer.Add(new System.Net.Cookie
+            {
+                Name = "JSESSIONID",
+                Value = JsessionidCookie,
+                Domain = "brs.urfu.ru",
+                Path = "/mrd"
+            });
+            using var client = new HttpClient(new HttpClientHandler()
+            {
+                CookieContainer = cookieContainer
+            });
+            var res = await client.GetStringAsync(url);
+            JArray disciplinesArray;
+            if (its)
+            {
+                disciplinesArray = JsonConvert.DeserializeObject<JArray>(res)!;
+            }
+            else
+            {
+                var response = JsonConvert.DeserializeObject<JObject>(res);
+                disciplinesArray = (JArray)response?["content"]!;
+            }
+            var disciplines = disciplinesArray
+                .Select(obj => new TechCardItem(
+                    obj["discipline"]!.Value<string>()!,
+                    obj["disciplineLoad"]!.Value<string>()!,
+                    obj["agreed"]!.Value<bool>()!
+                    ));
+            return disciplines.ToList();
+
+        }
+
+        private static async Task SelectExamLoadType(IPage page)
+        {
+            var form = page.Locator(".k-input");
+            var element = form.GetByText("Выберите вид занятий...");
+            var count = await element.CountAsync();
+            Console.WriteLine(count);
+            if (count == 0)
+                return;
+            await element.First.ClickAsync();
+
+            var lectures = page.Locator(".km-popup").GetByText("лекции");
+            await lectures.ClickAsync();
+        }
+
+        private static async Task OpenNotConfirmedCourse(IPage page, int index)
+        {
+            var candidate = page.GetByText("Нет");
+            await candidate.Nth(index).ClickAsync();
+            Console.WriteLine(await page.TitleAsync());
+        }
+
 
         private static async Task OpenLoadTypeSettings(IPage page, int loadTypeIndex)
         {
@@ -160,4 +250,6 @@ namespace tech_cards_creator
             await editBox.TypeAsync(value);
         }
     }
+
+    internal record TechCardItem(string CourseName, string Id, bool Agreed);
 }
