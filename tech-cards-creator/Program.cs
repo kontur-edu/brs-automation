4fb5b713d76748467fc782915c871dd4eb1a0dd5
Домашних Иван <digi@skbkontur.ru>
2024-03-27 13:34:30 +0500

    Ignoring Predicate

--- tech-cards-creator/Program.cs
+++ tech-cards-creator/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.IO;
+﻿using System.Globalization;
 using System.Net;
 using Microsoft.Playwright;
 using Newtonsoft.Json;
@@ -14,34 +12,74 @@ namespace tech_cards_creator
         // Нужно залогиниться вручную в браузере и скопировать из DevTools - Application - Cookie JSESSIONID:
         public const string JsessionidCookie = "";
 
+        // Год, в котором начался учебный год. 2023 для учебного года 2023/2024
+        public const int academicYear = 2023;
+
+        // Семестр. 1 для осеннего или 2 для весеннего
+        public const int semester = 2;
+
+        // Год обучения студентов. 1-4
+        public const int courseYear = 2;
+
+        // Обычные дисциплины или ИТС
+        public const bool its = false;
+
+        // Черный список дисциплин
+        private static readonly string[] whitelist = {
+            "Программирование галактик",
+            "Наблюдение вселенных"
+        };
+
+        // Белый список дисциплин
+        private static readonly string[] blacklist = {
+            "Теория игр (онлайн-курс)",
+        };
+
+        // Что делать с остальными дисциплинами? true - игнорировать, false - допустить
+        private const bool ignoreGraylist = its; // дисциплины ИТС точно надо игнорировать
+
+
+        // Условие на пропуск дисциплины
+        private static Func<TechCardItem, bool> ignoreCondition = techCard =>
+        {
+            // Дисциплины из черного списка всегда игнорируем
+            if (blacklist.Any(it => techCard.Discipline.StartsWith(it)))
+                return true;
+
+            // Дисциплины из белого списка всегда пропускаем
+            if (whitelist.Any(it => techCard.Discipline.StartsWith(it)))
+                return false;
+
+            // А что делать с остальными дисциплинами?
+            return ignoreGraylist;
+        };
+
         // На будущее: вероятно лучше полностью перейти на использование API.
         // Потому что фронт нечеловечески неудобен для его обхода через любые средства автоматизации браузеров :(
         // См, как реализовано получение всех дисциплин GetDisciplineTechCardsViaApi
-
-        static async Task Main2()
-        {
-            using var client = CreateHttpClient();
-            var res = await client.GetStringAsync($"https://brs.urfu.ru/mrd/mvc/mobile/discipline/fetch?year={2022}&termType={1}&course={3}&total=43&page=1&pageSize=300&search=%D0%A1%D0%BF%D0%B5%D1%86%D0%B8%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9");
-            var ans = JsonConvert.DeserializeObject<JObject>(res);
-            var disciplines = (JArray)ans["content"];
-            foreach (var discipline in disciplines)
-            {
-                discipline["group"] = discipline["group"].ToString().Replace(" ", "");
-            }
-        }
-
         public static async Task Main()
         {
-            var techCards = await GetDisciplineTechCardsViaApi(2022, 2, 2, false);
+            // Получение или установка браузера
             var context = await CreateBrowserContext();
+
+            // Получение списка техкарт
+            var techCards = await GetDisciplineTechCardsViaApi(academicYear, semester, courseYear, its);
+            
             var page = await context.NewPageAsync();
             await page.GotoAsync("https://brs.urfu.ru/mrd/mvc/mobile#/");
             await AddAuthCookie(context); // можно добавить куки, только когда мы уже открыли какую-то страницу в нужном домене.
             foreach (var techCard in techCards)
             {
-                // Вот этот if надо править, если хочется делать техкарты для чего-то другого:
-                if (techCard.Discipline.StartsWith("Специальный курс") || techCard.Agreed)
+                // Согласованные техкарты не стоит трогать
+                if (techCard.Agreed)
                     continue;
+
+                // Есть курсы, которые не согласуются через мобильную версию
+                if (ignoreCondition(techCard))
+                {
+                    Console.WriteLine("Ignoring tech card for course: " + techCard);
+                    continue;
+                }
 
                 Console.WriteLine("Create tech card for course: " + techCard);
                 await CreateTechCard(techCard, page);
